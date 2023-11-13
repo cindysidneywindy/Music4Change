7cd971d5abd989c289beabb716aa752a9add4aa9
“cindysidneywindy” <cindyxinxinwangtzx@gmail.com>
2023-11-12 16:46:09 -0800

    added carousel files.

---
+++ js/test.js
@@ -0,0 +1,69 @@
+const slides=document.getElementsByClassName("slide");
+let currentSlide= 0;
+
+const SlideButton = document.getElementById("slide-button");
+
+let firstSlide = true;
+
+function prevSlide(index) {
+    if (currentSlide < (slides.length)-2){
+            return (index === currentSlide+(slides.length-2));
+
+    }
+
+    return (index=== currentSlide - (slides.length-2));
+
+
+} 
+
+
+
+let currentOffsets = []
+
+function offsetfunction(){
+    for (let i=0; i<slides.length; i++){
+        currentOffsets.push(i*100);
+    }
+
+}
+
+
+function changeSlide(){
+    console.log(slides)
+    for (let i = 0; i<slides.length; i++) {
+        if (firstSlide){
+            slides[i].style.transition = "transform 0.5s";
+            slides[i].style.transform = "translateX(-100%)";
+            offsetfunction();
+            
+        }
+        else{
+            if (prevSlide(i)){
+                const maxOffset = 100*(slides.length-2);
+                const initialOffset = i*100;
+                slides[i].style.transition = "none";
+                slides[i].style.transform = `translateX(${maxOffset-initialOffset}%)`;
+                currentOffsets[i] = maxOffset-initialOffset;
+                console.log(currentSlide);
+                console.log(slides.length);
+            }
+            else{
+                const currentOffset = currentOffsets[i];
+                slides[i].style.transition = "transform 0.5s";
+                slides[i].style.transform = `translateX(${currentOffset-100}%)`;
+                currentOffsets[i] = currentOffset-100;
+                console.log(currentOffsets);
+            }
+        }
+    }
+    firstSlide=false;
+    
+    currentSlide=currentSlide+1;
+    if (currentSlide === slides.length){
+        currentSlide = 0;
+    }
+
+    console.log(currentSlide);
+}
+
+// SlideButton.addEventListener("click", changeSlide);
